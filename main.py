db0b76ce7cd29a2c0d86bb92a9494cc2dcc097dd
KTerhuja <132392099+KTerhuja@users.noreply.github.com>
2023-12-09 11:41:05 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import streamlit as st
-from streamlit-lottie import *
+import json
+import time
 
-lottie_url = 'https://assets5.lottiefiles.com/packages/lf20_V9t630.json'
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+
+lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
 
 st_lottie(lottie_url, key="user")
