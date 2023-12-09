372af0fff3f190d665c4197d34295d961bd8de33
KTerhuja <132392099+KTerhuja@users.noreply.github.com>
2023-12-09 11:47:57 +0000

    .

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
 
-lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
 
-st_lottie(lottie_url, key="user")
+lottie_file = load_lottiefile('cat-lottie-file.json')
+
+
+# lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
+
+st_lottie(lottie_file, key="user")
