66995f224838990f6e09ac6a9ff34f37adb073e0
KTerhuja <132392099+KTerhuja@users.noreply.github.com>
2023-12-09 15:42:53 +0000

    .

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import time
+import random 
+
 
 import requests
 import streamlit as st
@@ -9,11 +11,29 @@ from streamlit_lottie import st_lottie_spinner
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
-        return json.load(f)
+        return json.load(f)''
 
-lottie_file = load_lottiefile('cat-lottie-file.json')
+lottie_file = load_lottiefile('animations/love.json')
+
+with open ('pickup.txt', 'r') as f:
+
+    text = f.readlines()
 
 
 # lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
 
 st_lottie(lottie_file, key="user")
+
+
+def get_rizz():
+
+    rizz = random.choices(text)
+    rizz = rizz[0].strip('\n')
+
+    return rizz
+
+
+st.button("Reset", type="primary")
+if st.button('Say hello'):
+    rizz = get_rizz()
+    st.write(rizz)
---
+++ utils.py
@@ -0,0 +1,7 @@
+import random 
+
+with open ('pickup.txt', 'r') as f:
+
+    text = f.readlines()
+
+text
